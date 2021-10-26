103ff4a5945f8450534c7d77d55985d0cfa4ade9
Suhag Al Amin <developersuhag@gmail.com>
2021-10-26 16:02:46 +0600

    font color

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-import logo from "./logo.svg";
-import "./App.css";
-import "@fontsource/roboto/400.css";
 import "@fontsource/poppins/500.css";
 import "@fontsource/poppins/700.css";
+import "@fontsource/roboto/400.css";
+import {
+  Button,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
+import "./App.css";
+
+const theme = createTheme({
+  palette: {
+    // mode: "dark",
+    primary: {
+      main: "#00FFFF",
+    },
+    secondary: {
+      main: "#F9E7E7",
+    },
+    info: {
+      main: "#febc10",
+    },
+  },
+  background: {
+    default: "#222222",
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <h3>Dynamx a web startup</h3>
-    </div>
+    <CssBaseline>
+      <div className="App">
+        <ThemeProvider theme={theme}>
+          <Typography variant="h2" sx={{ color: "#00FFFF" }}>
+            Dynamx a web startup
+          </Typography>
+          <Button variant="outlined" color="secondary">
+            Click me
+          </Button>
+        </ThemeProvider>
+      </div>
+    </CssBaseline>
   );
 }
 
