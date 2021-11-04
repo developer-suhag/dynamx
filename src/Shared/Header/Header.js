f8511bb996d4ddc18a403606154d70d5aea2a8d1
Suhag Al Amin <developersuhag@gmail.com>
2021-11-04 20:43:28 +0600

    mobile bg

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,139 +1,158 @@
-import { AppBar, IconButton,Button, Box, Toolbar, Typography, Container } from '@mui/material';
-import { MenuItem, Menu , useMediaQuery } from "@material-ui/core";
+import {
+  AppBar,
+  IconButton,
+  Button,
+  Box,
+  Toolbar,
+  Typography,
+  Container,
+} from "@mui/material";
+import { MenuItem, Menu, useMediaQuery } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import { Link } from "react-router-dom";
-import React from 'react';
-import logo from "../../images/logo.png"
+import React from "react";
+import logo from "../../images/logo.png";
 
-const Header = ({classes, theme}) => {
-    const [anchor, setAnchor] = React.useState(null);
-    const open = Boolean(anchor);
-    const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-    const handleMenu = (event) => {
-        setAnchor(event.currentTarget);
-      };
+const Header = ({ classes, theme }) => {
+  const [anchor, setAnchor] = React.useState(null);
+  const open = Boolean(anchor);
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const handleMenu = (event) => {
+    setAnchor(event.currentTarget);
+  };
 
-    return (
-        <AppBar style={{backgroundColor:"#101620"}}>
-            <Container>
-              <Toolbar>
-                <Typography
-                  component="p"
-                  color="secondary"
-                  className={classes.title}
+  return (
+    <AppBar sx={{ bgcolor: "#101620", position: "inherit" }}>
+      <Container>
+        <Toolbar>
+          <Typography component="p" color="secondary" className={classes.title}>
+            <img src={logo} alt="" style={{ width: "180px" }} />
+          </Typography>
+          {isMobile ? (
+            <>
+              <IconButton
+                color="secondary"
+                className={classes.menuButton}
+                edge="start"
+                aria-label="menu"
+                onClick={handleMenu}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchor}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                KeepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={open}
+              >
+                <MenuItem
+                  onClick={() => setAnchor(null)}
+                  component={Link}
+                  to="/home"
                 >
-                  <img src={logo} alt="" style={{width:"180px"}} />
-                </Typography>
-                {isMobile ? (
-                  <>
-                    <IconButton
-                      color="secondary"
-                      className={classes.menuButton}
-                      edge="start"
-                      aria-label="menu"
-                      onClick={handleMenu}
-                    >
-                      <MenuIcon />
-                    </IconButton>
-                    <Menu
-                      id="menu-appbar"
-                      anchorEl={anchor}
-                      anchorOrigin={{
-                        vertical: "top",
-                        horizontal: "right"
-                      }}
-                      KeepMounted
-                      transformOrigin={{
-                        vertical: "top",
-                        horizontal: "right"
-                      }}
-                      open={open}
-                    >
-                      <MenuItem
-                        onClick={() => setAnchor(null)}
-                        component={Link}
-                        to="/home"
-                      >
-                        <Typography variant="h6" color="primary"> Home </Typography>
-                      </MenuItem>
-                      <MenuItem
-                        onClick={() => setAnchor(null)}
-                        component={Link}
-                        to="/services"
-                      >
-                        <Typography variant="h6" color="primary"> Services </Typography>
-                      </MenuItem>
-                      <MenuItem
-                        onClick={() => setAnchor(null)}
-                        component={Link}
-                        to="/blog"
-                      >
-                        <Typography variant="h6" color="primary"> Blog </Typography>
-                      </MenuItem>
-                      <MenuItem
-                        onClick={() => setAnchor(null)}
-                        component={Link}
-                        to="/about"
-                      >
-                        <Typography variant="h6" color="primary"> About </Typography>
-                      </MenuItem>
-                      <MenuItem
-                        onClick={() => setAnchor(null)}
-                        component={Link}
-                        to="/contact"
-                      >
-                        <Typography variant="h6" color="primary"> Contact </Typography>
-                      </MenuItem>
-                    </Menu>
-                  </>
-                ) : (
-                  <Box style={{ marginRight: "2rem"}}>
-                    <Button
-                      variant="text"
-                      component={Link}
-                      to="/home"
-                      color="primary"
-                    >
-                      Home
-                    </Button>
-                    <Button
-                      variant="text"
-                      component={Link}
-                      to="/services"
-                      color="primary"
-                    >
-                      Services
-                    </Button>
-                    <Button
-                      variant="text"
-                      component={Link}
-                      to="/blog"
-                      color="primary"
-                    >
-                      Blog
-                    </Button>
-                    <Button
-                      variant="text"
-                      component={Link}
-                      to="/about"
-                      color="primary"
-                    >
-                      About
-                    </Button>
-                    <Button
-                      variant="text"
-                      component={Link}
-                      to="/contact"
-                      color="primary"
-                    >
-                      Contact
-                    </Button>
-                  </Box>
-                )}
-              </Toolbar>
-            </Container>
-          </AppBar>
-    );
+                  <Typography variant="h6" color="primary">
+                    {" "}
+                    Home{" "}
+                  </Typography>
+                </MenuItem>
+                <MenuItem
+                  onClick={() => setAnchor(null)}
+                  component={Link}
+                  to="/services"
+                >
+                  <Typography variant="h6" color="primary">
+                    {" "}
+                    Services{" "}
+                  </Typography>
+                </MenuItem>
+                <MenuItem
+                  onClick={() => setAnchor(null)}
+                  component={Link}
+                  to="/blog"
+                >
+                  <Typography variant="h6" color="primary">
+                    {" "}
+                    Blog{" "}
+                  </Typography>
+                </MenuItem>
+                <MenuItem
+                  onClick={() => setAnchor(null)}
+                  component={Link}
+                  to="/about"
+                >
+                  <Typography variant="h6" color="primary">
+                    {" "}
+                    About{" "}
+                  </Typography>
+                </MenuItem>
+                <MenuItem
+                  onClick={() => setAnchor(null)}
+                  component={Link}
+                  to="/contact"
+                >
+                  <Typography variant="h6" color="primary">
+                    {" "}
+                    Contact{" "}
+                  </Typography>
+                </MenuItem>
+              </Menu>
+            </>
+          ) : (
+            <Box sx={{ marginRight: "2rem" }}>
+              <Button
+                variant="text"
+                component={Link}
+                to="/home"
+                color="primary"
+              >
+                Home
+              </Button>
+              <Button
+                variant="text"
+                component={Link}
+                to="/services"
+                color="primary"
+              >
+                Services
+              </Button>
+              <Button
+                variant="text"
+                component={Link}
+                to="/blog"
+                color="primary"
+              >
+                Blog
+              </Button>
+              <Button
+                variant="text"
+                component={Link}
+                to="/about"
+                color="primary"
+              >
+                About
+              </Button>
+              <Button
+                variant="text"
+                component={Link}
+                to="/contact"
+                color="primary"
+              >
+                Contact
+              </Button>
+            </Box>
+          )}
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default Header;
